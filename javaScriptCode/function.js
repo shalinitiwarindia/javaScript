48dab550f1650ff7b36b60493a8cf214a27638ed
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-06-28 21:11:59 +0530

    Create function.js

---
+++ javaScriptCode/function.js
@@ -0,0 +1,9 @@
+function showMessage() {
+  let message = "Hello, I'm JavaScript!"; // local variable
+
+  alert( message );
+}
+
+showMessage(); // Hello, I'm JavaScript!
+
+alert( message ); // <-- Error! The variable is local to the function
