7326e97592c9cd50e308f4de8178bc85fb267b0c
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-07-07 19:23:28 +0530

    Create User Properties.js

---
+++ javaScriptCode/User Properties.js
@@ -0,0 +1,12 @@
+let user = {
+  name: "John",
+  age: 30,
+  isAdmin: true
+};
+
+for (let key in user) {
+  // keys
+  alert( key );  // name, age, isAdmin
+  // values for the keys
+  alert( user[key] ); // John, 30, true
+}
