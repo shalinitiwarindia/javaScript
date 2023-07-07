763704c884cfd1161ad255d26eea8bb9d99ea359
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2023-07-07 19:05:11 +0530

    Create Property value shorthand.js

---
+++ javaScriptCode/Property value shorthand.js
@@ -0,0 +1,11 @@
+function makeUser(name, age) {
+  return {
+    name: name,
+    age: age,
+    // ...other properties
+  };
+}
+
+let user = makeUser("John", 30);
+alert(user.name); 
+alert(user.age);
